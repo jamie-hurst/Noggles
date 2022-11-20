ce6c904bb4c5c1de456e50bd7c955bffa5f4638b
warmrome <pxpx786@gmail.com>
2022-11-20 12:59:37 -0800

    Initial Commit

---
+++ Noggles/NogglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NogglesApp.swift
+//  Noggles
+//
+//  Created by Jameson Hurst on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct NogglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
