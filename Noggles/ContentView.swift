99fa0f46316bc850be4293dff5f34ea08d8fd37b
warmrome <pxpx786@gmail.com>
2022-12-18 19:59:13 -0800

    Adding haptics to the app

--- Noggles/ContentView.swift
+++ Noggles/ContentView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 
 struct ContentView: View {
     @State private var buttonText = "⌐◨-◨"
+    @State private var engine: CHHapticEngine?
     
     private let pasteboard = UIPasteboard.general
     private let noggles = ["⌐◨-◨", "◧-◧¬", "⌐◧-◧", "⌐⁞⁚⁞-⁞⁚⁞", "⌐◫-◫", "⌐◪-◪", "⌐◮-◮", "⌐♥-♥", "⌐🄶-🄼", "⌐ⓝ°ⓒ"]
@@ -27,6 +29,7 @@ struct ContentView: View {
                     ForEach(noggles, id: \.self) { noggle in
                         ZStack {
                             Button(noggle) {
+                                complexSuccess()
                                 copyToClipboard(noggle: noggle)
                             }
                             .buttonStyle(NogglesButton())
@@ -35,6 +38,7 @@ struct ContentView: View {
                     }
                 }
             }
+            .onAppear(perform: prepareHaptics)
             .background(.nounsRed)
             .navigationTitle(buttonText)
             .navigationBarTitleDisplayMode(.inline)
@@ -44,13 +48,46 @@ struct ContentView: View {
         
     }
     
-    
+    // Copy Functionality
     func copyToClipboard(noggle: String) {
         pasteboard.string = noggle
         buttonText = "copied ✓"
         // After 1.5 seconds, change the title back to the original text
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             buttonText = "⌐◨-◨"
+        }
+    }
+    
+    // Haptics
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // sculpt the tap and create an event
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 3)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.2)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
     
